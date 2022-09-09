1226ce043b6d41129c3b4876da9bd6b9a7717212
Dominik Korsa <dominik.korsa@gmail.com>
2022-09-09 21:51:35 +0200

    Add eslint

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,13 @@
 export function removePrefix(val: string, prefix: string) {
-    if (val.startsWith(prefix)) return val.substring(prefix.length);
-    return undefined;
+  if (val.startsWith(prefix)) return val.substring(prefix.length);
+  return undefined;
 }
 
 export function ifDefined<T, R = void>(val: T | undefined, fun: (val: T) => R): R | undefined {
-    if (val === undefined) return;
-    return fun(val);
+  if (val === undefined) return undefined;
+  return fun(val);
 }
 
 export function defined<T>(val: T | undefined): val is T {
-    return val !== undefined;
+  return val !== undefined;
 }
